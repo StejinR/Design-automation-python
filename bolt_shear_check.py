531bf5db9d1162574e0966d5e7bf5839037739a0
Stejin R <yrstejin@gmail.com>
2025-12-17 22:32:37 +0530

    Add bolt shear check script

---
+++ bolt_shear_check.py
@@ -0,0 +1,15 @@
+import math
+
+def bolt_shear_check(load_n, bolt_diameter_mm, allowable_shear_mpa):
+    area = math.pi * (bolt_diameter_mm ** 2) / 4
+    shear_stress = load_n / area
+    return round(shear_stress, 2), shear_stress <= allowable_shear_mpa
+
+load = 10000        # N
+diameter = 10       # mm
+allowable_stress = 60  # MPa
+
+stress, safe = bolt_shear_check(load, diameter, allowable_stress)
+
+print("Shear stress (MPa):", stress)
+print("Bolt safe:", safe)
